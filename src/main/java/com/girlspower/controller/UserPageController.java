fdabc9299b03dd1334e0d2b77c0ef836ccde96dc
AnBrusn <brusnitsyna.an@yandex.ru>
2019-12-14 17:03:56 +0300

    Add BMI and color progress bar

--- src/main/java/com/girlspower/controller/UserPageController.java
+++ src/main/java/com/girlspower/controller/UserPageController.java
@@ -24,6 +24,25 @@ public class UserPageController {
         model.put("username", user.getInfo().getFirstName());
         model.put("info", user.getInfo());
         model.put("dayTip", tipsService.getTodayAdvice());
+        double BMI = user.getInfo().getWeight() / Math.pow(user.getInfo().getHeight(), 2);
+        model.put("BMI", (double)Math.round(BMI * 100) / 100);
+        String descr;
+        if (BMI < 16) {
+            descr = "дефицит массы тела";
+        } else if (BMI < 18.5) {
+            descr = "недостаточная масса тела";
+        } else if (BMI < 25) {
+            descr = "нормальная масса тела :) ";
+        } else if (BMI < 30) {
+            descr = "избыточная масса тела";
+        } else if (BMI < 35) {
+            descr = "ожирение первой степени";
+        } else if (BMI < 40) {
+            descr = "ожирение второй степени";
+        } else {
+            descr = "ожирение третьей степени";
+        }
+        model.put("descr", descr);
         return "/cabinet";
     }
 }
